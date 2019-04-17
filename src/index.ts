8c8c6a6d8146a7a8b6906b678a29381b14786abd
jpopxfile <raynold.sim@gmail.com>
2019-04-17 19:48:37 +0900

    Add body check (#1)
    * Add body check
    
    * new line

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,12 @@ app.get('/health', (_, res) => {
 })
 
 app.post('/new', async (req, res) => {
+  if (!req.body || typeof req.body !== 'object') {
+    return res.status(400).send({
+      message: 'Error: missing or invalid request body'
+    })
+  }
+
   const { bridge, topic, type, token, peerName, language } = req.body
 
   if (!topic || typeof topic !== 'string') {
@@ -89,6 +95,12 @@ app.post('/new', async (req, res) => {
 })
 
 app.post('/push', async (req, res) => {
+  if (!req.body || typeof req.body !== 'object') {
+    return res.status(400).send({
+      message: 'Error: missing or invalid request body'
+    })
+  }  
+  
   const { topic } = req.body
 
   if (!topic || typeof topic !== 'string') {
